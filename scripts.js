27a1b725689d0868a3198a5228bfedcbcf09f8f3
Brennan Meret <brennanmeret@gmail.com>
2026-05-20 13:51:08 +0200

    Add email sending functionality with mailto link

---
+++ scripts.js
@@ -0,0 +1,14 @@
+document.getElementById("sendMail").addEventListener("click", function () {
+
+    const nom = encodeURIComponent(document.getElementById("name").value);
+    const emailUser = encodeURIComponent(document.getElementById("email").value);
+    const objet = encodeURIComponent(document.getElementById("objet").value);
+    const message = encodeURIComponent(document.getElementById("message").value);
+
+    const destinataire = "brennanmeret@gmail.com";
+    const sujet = encodeURIComponent("Contact depuis le site");
+
+    const body = `Nom Prénom : ${nom}%0AEmail : ${emailUser}%0AObjet : ${objet}%0A%0AMessage :%0A${message}`;
+
+    window.location.href = `mailto:${destinataire}?subject=${sujet}&body=${body}`;
+});
